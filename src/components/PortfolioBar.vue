857e72d3a132e95c6cb78b09f75ad13f084d2a2f
Антон Степаненков <lens@MacBook-Air-Anton-2.local>
2021-11-15 14:16:59 +0300

    feat: profiles tooltips

---
+++ src/components/PortfolioBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    This is a bar component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioBar',
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,13 +1,22 @@
 <template>
-  <h2>Portfolio</h2>
+  <v-row>
+    <portfolio-map></portfolio-map>
+    <portfolio-bar></portfolio-bar>
+    <portfolio-pie-chart></portfolio-pie-chart>
+  </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import PortfolioMap from '@/components/PortfolioMap'
+import PortfolioBar from '@/components/PortfolioBar'
+import PortfolioPieChart from '@/components/PortfolioPieChart'
 export default {
   name: 'Portfolio',
   components: {
+    PortfolioMap,
+    PortfolioBar,
+    PortfolioPieChart
   }
 }
 </script>
