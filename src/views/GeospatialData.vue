1d0893f122ecb86e4911ca317466b7c73873f15a
Антон Степаненков <lens@MacBook-Air-Anton-2.local>
2021-11-29 14:14:18 +0300

    feat: basic map example

--- src/views/GeospatialData.vue
+++ src/views/GeospatialData.vue
@@ -1,13 +1,48 @@
 <template>
-  <h2>Geospatial Data</h2>
+  <div class="">
+    <v-select
+      :items="names"
+      item-text="NAME_0"
+      item-value="bb"
+      label="Slelct country"
+      @change="selectCountry"
+      solo
+    ></v-select>
+    <div id="map">
+
+    </div>
+  </div>
 </template>
 
 <script>
+
+import names from '@/gis/static/names';
+import GIS from '@/gis'
 // @ is an alias to /src
 
 export default {
   name: 'GeospatialData',
+  data() {
+    return {
+      names: names,
+      map:null
+    }
+  },
   components: {
+  },
+  methods: {
+    selectCountry(selection) {
+      this.map.zoomTo(selection);
+    }
+  },
+  mounted() {
+    this.map = new GIS('map');
   }
 }
 </script>
+<style media="screen">
+  #map{
+    height: 100vh;
+    width: 100%;
+  }
+</style>
