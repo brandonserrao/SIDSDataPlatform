bfe88c41b8a1b45b1730e8f66d380fe9141c55fa
Антон Степаненков <lens@MacBook-Air-Anton-2.local>
2022-01-27 18:22:54 +0300

    new build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/SIDSDataPlatform/'
-    : '/',
+  publicPath: '/SIDSDataPlatform/',
   transpileDependencies: [
     'vuetify'
   ]
