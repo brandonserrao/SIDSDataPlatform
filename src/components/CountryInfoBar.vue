84054891df0416863f45e585db1bf40e514c63d5
Brandon Serrao <brandon.serrao@outlook.com>
2022-02-15 03:53:14 -0400

    added example chrome tabs to data controller

--- src/components/CountryInfoBar.vue
+++ src/components/CountryInfoBar.vue
@@ -2,68 +2,117 @@
   <v-card flat class="overflow background-grey">
     <v-row dense>
       <v-col cols="7" md="4">
-      <v-subheader class="d-none d-md-block info-bar_header block-header">{{name}}</v-subheader>
-        <v-list dense class="indicators-list background-grey"
-        >
+        <v-subheader class="d-none d-md-block info-bar_header block-header">{{
+          name
+        }}</v-subheader>
+        <v-list dense class="indicators-list background-grey">
           <v-list-item v-if="checkIndicator('key-1')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-1'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-1').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-1'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-1").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-2')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-2'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-2').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-2'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-2").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-3')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-3'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-3').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-3'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-3").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-wdi2-SP.POP.TOTL')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-wdi2-SP.POP.TOTL'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{nFormatter(getIndicator('key-wdi2-SP.POP.TOTL').value)}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-wdi2-SP.POP.TOTL'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                nFormatter(getIndicator("key-wdi2-SP.POP.TOTL").value)
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-7')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-7'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-7').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-7'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-7").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-wdi-AG.SRF.TOTL.K2')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-wdi-AG.SRF.TOTL.K2'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{nFormatter(getIndicator('key-wdi-AG.SRF.TOTL.K2').value)}} km<sup>2</sup></v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-wdi-AG.SRF.TOTL.K2'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle
+                >{{
+                  nFormatter(getIndicator("key-wdi-AG.SRF.TOTL.K2").value)
+                }}
+                km<sup>2</sup></v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-10')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-10'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-10').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-10'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-10").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-hdr-137506')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-hdr-137506'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{computeHDI(getIndicator('key-hdr-137506').value)}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-hdr-137506'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                computeHDI(getIndicator("key-hdr-137506").value)
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-wdi2-SI.POV.GINI')">
             <v-list-item-content class="one-line">
-              <v-list-item-title class="one-line_header" v-text="indicatorsMetadata['key-wdi2-SI.POV.GINI'].indicator"></v-list-item-title>
-              <v-list-item-subtitle>{{getIndicator('key-wdi2-SI.POV.GINI').value}}</v-list-item-subtitle>
+              <v-list-item-title
+                class="one-line_header"
+                v-text="indicatorsMetadata['key-wdi2-SI.POV.GINI'].indicator"
+              ></v-list-item-title>
+              <v-list-item-subtitle>{{
+                getIndicator("key-wdi2-SI.POV.GINI").value
+              }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-if="checkIndicator('key-13')">
             <v-list-item-content class="one-line">
               <v-list-item-title class="one-line_header">
                 <a :href="getIndicator('key-13').value" target="_blank">
-                  {{indicatorsMetadata['key-13'].indicator}}
+                  {{ indicatorsMetadata["key-13"].indicator }}
                 </a>
               </v-list-item-title>
             </v-list-item-content>
@@ -91,37 +140,44 @@
 </template>
 
 <script>
-import format from '@/mixins/format.mixin'
-import { mapState } from 'vuex';
+import format from "@/mixins/format.mixin";
+import { mapState } from "vuex";
 
 export default {
-  name: 'CountryInfoBar',
-  props: ['profile', 'id', 'name'],
-  mixins:[format],
-  computed:{
+  name: "CountryInfoBar",
+  props: ["profile", "id", "name"],
+  mixins: [format],
+  computed: {
     ...mapState({
-      indicatorsMetadata: state => state.profiles.indicatorsMetadata
-    })
+      indicatorsMetadata: (state) => state.profiles.indicatorsMetadata,
+    }),
   },
   methods: {
-    computeHDI(hdi){
-      let hdiNum=parseFloat(hdi),
-      hdiClass = "No data";
-      if(hdi>=.8){hdiClass="Very high human development"}
-      else if(hdi>=0.7){hdiClass="High human development"}
-      else if(hdi>=0.550){hdiClass="Medium human development"}
-      else if(hdi>0){hdiClass="Low human development"}
-      else { hdiClass="No data"; hdiNum="No data"}
-      return `${hdiNum}, ${hdiClass}`
+    computeHDI(hdi) {
+      let hdiNum = parseFloat(hdi),
+        hdiClass = "No data";
+      if (hdi >= 0.8) {
+        hdiClass = "Very high human development";
+      } else if (hdi >= 0.7) {
+        hdiClass = "High human development";
+      } else if (hdi >= 0.55) {
+        hdiClass = "Medium human development";
+      } else if (hdi > 0) {
+        hdiClass = "Low human development";
+      } else {
+        hdiClass = "No data";
+        hdiNum = "No data";
+      }
+      return `${hdiNum}, ${hdiClass}`;
     },
     getIndicator(code) {
-      return this.profile.find(indicators => indicators.axis === code);
+      return this.profile.find((indicators) => indicators.axis === code);
     },
     checkIndicator(code) {
       return this.getIndicator(code) && this.getIndicator(code).value;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -140,7 +196,7 @@ export default {
   flex: 0 1 auto !important;
   margin-right: 8px !important;
   text-overflow: none;
-  overflow:visible;
+  overflow: visible;
 }
 .info-bar_header {
   padding-top: 10px;
